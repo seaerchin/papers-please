1fd8e8f900e0f828d3fa7b61afef0a3baaf1b89d
seaerchin <jiachin@open.gov.sg>
2024-05-14 02:30:55 +0800

    feat: 113
    working code, but very ugly

--- leetcode/113/113.py
+++ leetcode/113/113.py
@@ -5,25 +5,45 @@
 #         self.left = left
 #         self.right = right
 
+def isLeaf(root): 
+    if not root: return False
+    if root.left or root.right: return False 
+    return True
+
 # returns a list<list<int>>
-def walk(target, cur, root):
-    if not root and target == cur: 
-        return [[]] 
-    if not root: 
-        return []
-   
-    left_paths = walk(target - root.val, root.left)
-    right_paths = walk(target - root.val, root.right)
+# when we hit the root node, 
+# return a list of lists if 
+# we have the value required 
+# otherwise,
+def walk(target, root):
+    # terminate here as we already 
+    # hit the leaf nodes of the tree
+    if isLeaf(root):
+        if target - root.val == 0: 
+            return [[root.val]] 
+        else: 
+            return []
     
-    # need to map to each path returned
-    if len(left_paths) > 0:
-        cur_path.map(lambda x: x + [root.val])
-    if len(right_paths) > 0:
-        cur_path.map(lambda x: x + [root.val])
-    return cur_path
+    left_paths = [] 
+    right_paths = [] 
+
+    if (root.left): 
+        left_paths = walk(target - root.val,  root.left)
+    if (root.right): 
+        right_paths = walk(target - root.val, root.right)
+
+    ans = []
+    base = [[]] if left_paths == [[]] and right_paths == [[]] else left_paths + right_paths 
+
+    for val in iter(base):
+        cur_path = [root.val] + val 
+        ans.append(cur_path)
+
+    return ans 
 
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root: return []
         paths = walk(targetSum, root)
         return paths 
 
